f0ddca18212a7c0636b5ef0cd58cc52b1ac1cc26
Derlys Dominguez <derlys275@gmail.com>
2020-07-05 23:17:04 -0500

    Tutorial 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
-import { UiBrand} from '@kikstart-ui/ui-brand';
+import { UiBrand } from '@kikstart-ui/ui-brand';
+import { UiLink } from '@kikstart-ui/ui-link';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'site';
   brand: UiBrand = {
-    name: 'Site',
+    name: 'kikstart',
     logo: 'https://github.com/kikstart-ui.png',
   };
+  links: UiLink[] = [
+    { label: 'Home', path: '/' },
+    { label: 'About', path: '/about' },
+    { label: 'Contact', path: '/contact' },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutWebModule } from '@kikstart-ui/layout-web';
-import { UiNavbarModule} from '@kikstart-ui/ui-navbar';
+import { UiNavbarModule } from '@kikstart-ui/ui-navbar';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { UiHeroModule } from '@kikstart-ui/ui-hero';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomeComponent, AboutComponent, ContactComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     LayoutWebModule,
-    UiNavbarModule
+    UiNavbarModule,
+    UiHeroModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
